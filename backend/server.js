0254ee0b8481285fc834a8fc9e335347374b2bf5
Muhammed Saqlain A <mdsaqlain275@gmail.com>
2026-04-13 17:40:21 +0530

    auth feature with routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const authRoutes = require('./src/features/auth/auth.routes');
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.send('API running');
---
+++ backend/src/features/auth/auth.controller.js
@@ -0,0 +1,8 @@
+
+exports.login = (req, res) => {
+    res.json({ message: "Login successful" });
+};
+
+exports.register = (req, res) => {
+    res.json({ message: "Register successful" });
+};
